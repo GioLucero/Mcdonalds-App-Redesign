078d1ba896b3f70548a0c6b225d6db9f9df1d634
GioLucero <gioandrewlucero@gmail.com>
2019-12-06 12:23:10 -0500

    Initial Commit

---
+++ Mcdonalds App Redesign/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mcdonalds App Redesign
+//
+//  Created by Gio Lucero on 2019-12-06.
+//  Copyright © 2019 Gio Lucero. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
